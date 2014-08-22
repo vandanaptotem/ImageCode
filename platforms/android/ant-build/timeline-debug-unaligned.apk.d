854afb0e4b2023cddc98975cf48ad9c8adc121f5
saurabhbhandari2006 <saurabhbhandari2006@gmail.com>
2014-08-22 13:31:48 +0530

    ant-build removed

--- platforms/android/ant-build/timeline-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/home/saurabh/timline/platforms/android/ant-build/timeline-debug-unaligned.apk : \
-/home/saurabh/timline/platforms/android/ant-build/timeline.ap_ \
-/home/saurabh/timline/platforms/android/ant-build/classes.dex \
-/home/saurabh/timline/platforms/android/CordovaLib/ant-build/classes.jar \
